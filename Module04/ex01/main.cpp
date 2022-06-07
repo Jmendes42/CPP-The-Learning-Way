5e761beadf71fa2acc442ae6b9e097f6ca6f5cb7
joao-mendes723 <joao.brandao.mendes@gmail.com>
2022-06-07 22:44:23 +0100

    Module04 ex01 progress

--- Module04/ex01/main.cpp
+++ Module04/ex01/main.cpp
@@ -6,9 +6,26 @@
 
 int main()
 {
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    
+    int             size = 6;
+    const Animal    *j = new Dog();
+    const Animal    *i = new Cat();
+    const Animal    *array[size];
+
+    for (int x = 0; x < size; x++)
+    {
+        if (x < size / 2)
+            array[x] = new Cat();
+        else
+            array[x] = new Dog();
+    }
+
+    array[5]->makeSound();
+
+    //array[5] = i;
+
+    for (int x = 0; x < size; x++)
+        delete array[x];
+
     delete j;//should not create a leak
     delete i;
     return (0);
